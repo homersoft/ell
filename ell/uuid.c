c5ac41f46f8e424e416b68531b071ba6e37774c4
Michał 'Khorne' Lowas-Rzechonek <michal.lowas-rzechonek@silvair.com>
2019-01-17 14:25:37 +0100

    uuid: Use PRIx8 for better portability

--- ell/uuid.c
+++ ell/uuid.c
@@ -25,6 +25,7 @@
 #endif
 
 #define _GNU_SOURCE
+#include <inttypes.h>
 #include <stdio.h>
 
 #include "checksum.h"
@@ -210,14 +211,17 @@ LIB_EXPORT bool l_uuid_to_string(const uint8_t uuid[16],
 {
 	int n;
 
-	n = snprintf(dest, dest_size, "%02x%02x%02x%02x-%02x%02x-%02x%02x-"
-					"%02x%02x-%02x%02x%02x%02x%02x%02x",
-					uuid[0], uuid[1], uuid[2], uuid[3],
-					uuid[4], uuid[5],
-					uuid[6], uuid[7],
-					uuid[8], uuid[9],
-					uuid[10], uuid[11], uuid[12],
-					uuid[13], uuid[14], uuid[15]);
+	n = snprintf(dest, dest_size,
+			"%02" PRIx8 "%02" PRIx8 "%02" PRIx8 "%02" PRIx8 "-"
+			"%02" PRIx8 "%02" PRIx8 "-"
+			"%02" PRIx8 "%02" PRIx8 "-"
+			"%02" PRIx8 "%02" PRIx8 "-"
+			"%02" PRIx8 "%02" PRIx8 "%02" PRIx8 "%02" PRIx8 "%02" PRIx8 "%02" PRIx8 "",
+			uuid[0], uuid[1], uuid[2], uuid[3],
+			uuid[4], uuid[5],
+			uuid[6], uuid[7],
+			uuid[8], uuid[9],
+			uuid[10], uuid[11], uuid[12], uuid[13], uuid[14], uuid[15]);
 
 	if (n < 0 || (size_t) n >= dest_size)
 		return false;
